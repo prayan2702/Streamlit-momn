f1e22a7a1002e6b3d6ae55405cf0b7803a519f5a
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2024-12-14 22:00:14 +0530

    Update momn_streamlit_app.py

--- momn_streamlit_app.py
+++ momn_streamlit_app.py
@@ -271,10 +271,9 @@ if start_button:
         st.header("Menu")
 
         # Display the rebalance date
-        st.info(f"Next NSE Rebalance Date: **{formatted_rebalance_date}**.")
-        with st.expander("Applied Filters", expanded=False):
-            st.write("The following filters are applied:")
-            for i, filter_desc in enumerate(filters, start=1):
+        st.info(f"Index Rebalance Date: **{formatted_rebalance_date}**.")
+        st.write("### Applied Filters:")
+        for i, filter_desc in enumerate(filters, start=1):
                 st.write(f"{i}. {filter_desc}")
 
 #**************************************************
