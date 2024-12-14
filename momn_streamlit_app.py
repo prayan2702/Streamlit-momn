96958baa1460659478ec07595dc0aea76272d0ae
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2024-12-14 22:30:39 +0530

    Update momn_streamlit_app.py

--- momn_streamlit_app.py
+++ momn_streamlit_app.py
@@ -142,7 +142,7 @@ dates = {
     'date1M': datetime.strptime(dt2, '%Y-%m-%d') - relativedelta(months=1),
 }
 
-st.write("### Date Range:")
+st.write("##### Date Range:")
 st.write(f"Start Date: **{dates['startDate'].strftime('%d-%m-%Y')}**")
 st.write(f"End Date: **{dates['endDate'].strftime('%d-%m-%Y')}**")
 
