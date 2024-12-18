c6f1114ff4398fb52b3adf9c08a87e5dbfdb17e4
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2024-12-18 22:57:29 +0530

    Update momn_streamlit_app.py

--- momn_streamlit_app.py
+++ momn_streamlit_app.py
@@ -367,7 +367,8 @@ if start_button:
     dfStats['sharpe12M'] = dfStats['sharpe12M'].replace([np.inf, -np.inf], np.nan).fillna(0)
 
     # Add Rank column based on 'avgSharpe' and sort by Rank
-    dfStats['Rank'] = dfStats[ranking_method].rank(ascending=False,method='first').astype(int)
+    #dfStats['Rank'] = dfStats[ranking_method].rank(ascending=False,method='first').astype(int)
+    dfStats['Rank'] = dfStats[ranking_method].rank(ascending=False,method='first')
     dfStats = dfStats.sort_values('Rank').set_index('Rank')  # Set 'Rank' as index
 
     # Show both filtered and unfiltered data in Streamlit
