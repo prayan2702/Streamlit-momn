f5013e2a9b4b16d12c5c72f75600a856858389d7
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-21 00:58:30 +0530

    Update momn_streamlit_app.py

--- momn_streamlit_app.py
+++ momn_streamlit_app.py
@@ -223,8 +223,10 @@ if start_button:
         progress_percentage = int(progress * 100)
         status_text.text(f"Downloading... {progress_percentage}%")
 
-        # Add random delay (2–5 seconds) between chunks
-        time.sleep(random.uniform(2, 5))
+        # # Add random delay (2–5 seconds) between chunks
+        # time.sleep(random.uniform(2, 5))
+
+	time.sleep(0.5)
 
     # After the download is complete, update the progress bar and text
     progress_bar.progress(1.0)
