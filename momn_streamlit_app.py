561734fb899e8a60f7109cb54e242f8de06d0856
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-20 12:33:49 +0530

    Update momn_streamlit_app.py

--- momn_streamlit_app.py
+++ momn_streamlit_app.py
@@ -174,8 +174,8 @@ def download_chunk_with_retries(symbols, start_date, max_retries=3, delay=2):
             return yf.download(symbols, start=start_date, progress=False)
         except (Exception, JSONDecodeError, ValueError) as e:
             if attempt < max_retries - 1:
-                time.sleep(delay)
-		print(f"Retrying {symbol} (attempt {attempt + 1}/{max_retries})...")
+    		time.sleep(delay)
+    		print(f"Retrying {symbol} (attempt {attempt + 1}/{max_retries})...")
             else:
                 raise e
 
