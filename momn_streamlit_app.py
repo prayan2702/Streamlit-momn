44b27cd1ce816f13c627e120fd17354595933141
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-21 23:27:48 +0530

    Update momn_streamlit_app.py

--- momn_streamlit_app.py
+++ momn_streamlit_app.py
@@ -32,10 +32,10 @@ def login():
         st.markdown("""
             <style>
                 .login-form {
-                    max-width: 300px;
+                    max-width: 200px;
                     margin: 0 auto;
                     padding: 20px;
-                    border-radius: 6px;
+                    border-radius: 8px;
                     box-shadow: 0 4px 8px rgba(0,0,0,0.1);
                 }
             </style>
